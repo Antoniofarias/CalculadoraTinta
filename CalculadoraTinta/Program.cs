9ff01cc665e34a85cf08ad557cae1bbd3727c869
Antonio <antoniofjj@hotmail.com>
2019-02-27 14:11:25 -0300

    Commit

--- CalculadoraTinta/Program.cs
+++ CalculadoraTinta/Program.cs
@@ -10,6 +10,23 @@ namespace CalculadoraTinta
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Qual a largura do comodo?");
+            double largura = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qual a profundidade do comodo?");
+            double profundidade = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("A Area das Paredes : ");
+            const double Altura = 2.9;
+            Calculadora calculadora = new Calculadora();
+            Console.WriteLine(calculadora.CalcularAreaParedes(largura, profundidade, Altura));
+
+            Console.WriteLine("A Area do teto : ");
+            Console.WriteLine(calculadora.CalcularAreaTeto(largura, profundidade));
+
+            Console.WriteLine("A litragem de tinta necessaria e : ");
+            Console.WriteLine(calculadora.CalcularLitragemNecessaria());
+            Console.ReadLine();
         }
     }
 }
